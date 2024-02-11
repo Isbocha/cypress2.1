b77bdef3166fc88ff05b28b0ad60d695fdcdd7ba
InnaB <isbocha@gmail.com>
2024-02-11 21:11:42 +0300

    Add new test

--- cypress/e2e/tickets.cy.js
+++ cypress/e2e/tickets.cy.js
@@ -7,16 +7,16 @@ it("Should book a ticket", () => {
     cy.contains(selector.title1).should("be.visible");
     cy.contains(selector.title2).should("be.visible");
     cy.get('[draggable="true"][data-film-id="122"] > .conf-step__movie-title').should('have.text','Микки маус');
-    cy.get('[data-film-id="122"] ').invoke('text').then((text) => {
+    cy.get('[draggable="true"][data-film-id="122"] > .conf-step__movie-title').invoke('text').then((text) => {
     cy.visit('/');
-    cy.get(selector.movieTitle).should('have.text', 'Микки маус')
-//    cy.get(selector.chosenDay).click();
-//    cy.get(selector.chosenTime).click();
-//    cy.contains(selector.session).should("be.visible");
-//    cy.get(selector.chosenPlace).click();
-//    cy.get(selector.pushButton).click();
-//    cy.contains(selector.bookingСode).should("be.visible");
-//    cy.get(selector.pushButton).click();
-//    cy.contains(selector.electronicTicket).should("be.visible");
+    cy.get(selector.movieTitle).should('have.text', text)
+    cy.get(selector.chosenDay).click();
+    cy.get(selector.chosenTime).click();
+    cy.contains(selector.session).should("be.visible");
+    cy.get(selector.chosenPlace).click();
+    cy.get(selector.pushButton).click();
+    cy.contains(selector.bookingСode).should("be.visible");
+    cy.get(selector.pushButton).click();
+    cy.contains(selector.electronicTicket).should("be.visible");
 });
 })
